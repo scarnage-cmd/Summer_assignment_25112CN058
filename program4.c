43ef6040e9e5a0c9f6d5a65c6364b6b4602e96a9
Sanidhya <sanidhyatiwari005@gmail.com>
2026-05-27 14:57:52 +0530

    number of digits

---
+++ program4.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n ;
+    printf("Enter the number : ");
+    scanf("%d",&n);
+
+    int count =0;
+    int rem ;
+
+    for(int i =0;i<=n;i++)
+    {
+        rem = n%10;
+        count += 1;
+        n =n/10;
+    }
+
+    printf("The number of digits in the entered number : %d",count);
+    return 0;
+}
