d938f50e5ff294164004f3093885a61ddf4de916
Sanidhya <sanidhyatiwari005@gmail.com>
2026-05-27 12:58:04 +0530

    sum till n terms

--- program1.c
+++ program1.c
@@ -1,6 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    printf("HEllo");
+    int sum = 0 ;
+    int n ;
+    printf("Enter N : ");
+    scanf("%d",&n);
+
+    for(int i =0;i<=n;i++)
+    {
+        sum += i;
+    }
+
+    printf("The sum uptill n terms : %d",sum);
+
     return 0;
 }
