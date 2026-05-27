9a1e47771f63907352a96b727a1943b9a7824a1b
Sanidhya <sanidhyatiwari005@gmail.com>
2026-05-27 14:19:15 +0530

    multiplication

---
+++ program2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the number you want table of : ");
+    scanf("%d",&n);
+
+    int multiple;
+    printf("Enter the last multiple you want your table upto :");
+    scanf("%d",&multiple);
+
+    int table =1;
+    int i;
+
+    printf("The table of %d is as follows :-\n",n);
+
+    for(i=1 ;i<=multiple;i++)
+    {
+        table = n * i;
+        printf("%d * %d = %d \n",n,i,table);
+    }
+
+    return 0;
+}
